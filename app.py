d51fe7476b9dc37f542b0abbd6d6cfaf838c595a
rizkimaulana <rizkimaulana648@gmail.com>
2026-06-06 12:12:17 +0700

    fix sklearn version and debug mode

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import pandas as pd
 import joblib
 
@@ -39,4 +40,4 @@ def prediksi():
     return jsonify({"prediksi_waktu": int(hasil)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))s
