e56950cdd9109b036d76c286752686e795564368
rizkimaulana <rizkimaulana648@gmail.com>
2026-06-16 11:34:04 +0700

    update model dan app untuk dataset baru 1500 data

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ import pandas as pd
 import joblib
 
 app = Flask(__name__)
-model             = joblib.load("model_rf.pkl")
-encoder_jenis     = joblib.load("encoder_jenis.pkl")
-encoder_kebasahan = joblib.load("encoder_kebasahan.pkl")
-encoder_ketebalan = joblib.load("encoder_ketebalan.pkl")
+model         = joblib.load("model_rf.pkl")
+encoder_jenis = joblib.load("encoder_jenis.pkl")
 
 @app.route('/prediksi', methods=['POST'])
 def prediksi():
-    data       = request.json
-    jenis      = data["jenis_sepatu"]
-    suhu       = data["suhu"]
-    kelembapan = data["kelembapan"]
+    data = request.json
 
-    # ESP32 sudah kirim nama Inggris, tapi jaga-jaga
+    # Ambil field baru
+    jenis            = data["jenis_sepatu"]
+    suhu_awal        = data["suhu_awal"]
+    kelembapan_awal  = data["kelembapan_awal"]
+    suhu_akhir       = data["suhu_akhir"]
+    kelembapan_akhir = data["kelembapan_akhir"]
+
+    # Mapping nama jenis (jaga-jaga jika ESP32 kirim nama Indonesia)
     mapping = {
         "Kanvas": "Canvas",
         "Kulit":  "Leather",
@@ -24,16 +26,14 @@ def prediksi():
     }
     jenis = mapping.get(jenis, jenis)
 
-    jenis_enc     = encoder_jenis.transform([jenis])[0]
-    kebasahan_enc = encoder_kebasahan.transform(["basah"])[0]  # default
-    ketebalan_enc = encoder_ketebalan.transform(["sedang"])[0]  # default
+    jenis_enc = encoder_jenis.transform([jenis])[0]
 
     input_data = pd.DataFrame([{
-        "jenis_sepatu_enc":       jenis_enc,
-        "suhu":                   suhu,
-        "kelembapan":             kelembapan,
-        "tingkat_kebasahan_enc":  kebasahan_enc,
-        "ketebalan_bahan_enc":    ketebalan_enc
+        "jenis_sepatu_enc":  jenis_enc,
+        "suhu_awal":         suhu_awal,
+        "kelembapan_awal":   kelembapan_awal,
+        "suhu_akhir":        suhu_akhir,
+        "kelembapan_akhir":  kelembapan_akhir
     }])
 
     hasil = model.predict(input_data)[0]
